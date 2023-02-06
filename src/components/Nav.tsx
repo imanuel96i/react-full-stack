a34c3e66d69a83dab983f75a521921bfb8b5f621
Manuel Vidal <imanuel96i@gmail.com>
2023-02-06 17:32:07 -0300

    Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function Nav() {
-  return (<div className='NavBar'>
-      <div className='NavContainer'>Nav 1</div>
-      <div className='NavContainer'>Nav 2</div>
-      <div className='NavContainer'>Nav 3</div>
+  return (
+    <div className='NavBar'>
+      <div className='NavContainer'>
+        <h2>MetraCL</h2>
+      </div>
+      <div className='NavContainer'>
+        <input type="text" id='txt' placeholder='HOLA'/>
+      </div>
+      <div className='NavContainerLink'>
+        <a href='www.google.cl'>Home</a>
+        <a href='#k'>About</a>
+        <a href='#kk'>Contact</a>
+      </div>
     </div>
   )
 }
