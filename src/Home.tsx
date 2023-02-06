27139b853d3149501007f785b09a73fd7295ea0b
Manuel Vidal <imanuel96i@gmail.com>
2023-02-06 18:19:21 -0300

    Carousel

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Carousel from './components/Carousel'
 
 function Home() {
   return (<>
       <Nav />
+      <Carousel/>
     </>
   )
 }
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Carousel() {
+  return (
+    <div className='CarouselContainer'>
+      <div className='ArrowLeft'>ArrowLeft</div>
+      <div className='Arrow'>ArrowRight</div>
+    </div>
+  )
+}
+
+export default Carousel
