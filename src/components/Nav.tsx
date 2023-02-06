af3462d8000960381f94a513b526ab88d1fc344a
Manuel Vidal <imanuel96i@gmail.com>
2023-02-06 16:57:38 -0300

    Home + sass

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+  return (<div className='NavBar'>
+      <div className='NavContainer'>Nav 1</div>
+      <div className='NavContainer'>Nav 2</div>
+      <div className='NavContainer'>Nav 3</div>
+    </div>
+  )
+}
+
+export default Nav
