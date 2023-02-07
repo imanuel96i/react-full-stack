0f39d88befe00c17d3977dc7bf607f9ea66ff908
Manuel Vidal <imanuel96i@gmail.com>
2023-02-07 13:01:19 -0300

    Adicion card al home

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Nav from "./components/Nav";
 import Carousel from "./components/Carousel";
 
+import Card from "./components/Card"
+
 function Home() {
   return (
     <>
       <Nav />
       <Carousel />
+      <Card/>
     </>
   );
 }
