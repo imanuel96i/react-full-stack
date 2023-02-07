6bc4eb9bae2306de8c365b2910a482e540b9aa24
Manuel Vidal <imanuel96i@gmail.com>
2023-02-07 12:56:52 -0300

    Cards

---
+++ src/components/Card.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './css/Card.sass'
+import verano from './assets/img/sl-verano-escolares.jpg'
+
+function Card() {
+  return (
+      <div className='Container'>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+          <div className='Card'>
+              <h2>Verano</h2>
+              <img className='Img' src={verano} alt='' />
+              <p>HOLA ESTO ES EL VERANO</p>
+          </div>
+    </div>
+  )
+}
+
+export default Card
