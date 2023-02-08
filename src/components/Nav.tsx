793655bc8cbc75f65e6191c0df366f314d953ad9
Ignacio Korenhof <ig.korenhof@alumnos.duoc.cl>
2023-02-08 16:48:25 -0300

    Menu responsivo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './css/Nav.sass'
 import { FaBars } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md'
 
 function Nav() {
+
+  const [classContainerLink, setClassContainerLink] = useState('NavContainerLink unclicked')
+  const [displayMenuButton, setDisplayMenuButton] = useState(true);
+  const updateMenuButton = () =>{
+    setDisplayMenuButton(!displayMenuButton);
+    if(displayMenuButton === true){
+      setClassContainerLink('NavContainerLink clicked');
+    }
+    else{
+      setClassContainerLink('NavContainerLink unclicked');
+    }
+  };
   return (
     <div className='NavBar'>
       <div className='NavContainer'>
         <h2>MetraCL</h2>
-        <FaBars className='Bars'/>
+        { displayMenuButton ? (
+          <FaBars className='NavDisplayMenuButton' onClick={updateMenuButton}/>
+        ):
+          <MdClose className='NavDisplayMenuButton' onClick={updateMenuButton}/>
+        }
       </div>
       <div className='NavContainerSearch'>
-          <input type="text" id='txt' className='SearchBar' placeholder='HOLA'/>
+          <input type="text" id='txt' className='NavSearchBar' placeholder='HOLA'/>
       </div>
-      <div className='NavContainerLink'>
+      <div id='test' className={classContainerLink}>
         <a href='www.google.cl'>Home</a>
         <a href='#k'>About</a>
         <a href='#kk'>Contact</a>
