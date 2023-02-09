19db64508636db63c7720b4a2d397d116af63940
Benjaex2 <benjaex2@gmail.com>
2023-02-09 09:59:19 -0300

    footer humilde

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,56 @@
-import React from 'react'
-import './css/Footer.sass'
-import {AiFillFacebook, AiFillInstagram, AiFillLinkedin, AiFillTwitterSquare} from 'react-icons/ai'
+import React from "react";
+import "./css/Footer.sass";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
 
 function Footer() {
   return (
-    <div className='ContainerFoo'>
-      <div className="Footer">
-        <div className="Fcard">
-          INFO
-        </div>
-        <div className="Fcard flex-column">
-          <div className='Rssi'>Siguenos en nuestras redes sociales!</div>
-          <div>
-            <a href='https://es-la.facebook.com/' target="_blank" rel="noreferrer">
-              <AiFillFacebook className='Rrss' />
-            </a>
-            <a href='https://www.instagram.com/' target="_blank" rel="noreferrer">
-              <AiFillInstagram className='Rrss' />
-            </a>
-            <a href='https://www.linkedin.com/' target="_blank" rel="noreferrer">
-              <AiFillLinkedin className='Rrss' />
-            </a>
-            <a href='https://twitter.com/' target="_blank" rel="noreferrer">
-              <AiFillTwitterSquare className='Rrss' />
-            </a>
-          </div>
-        </div>
+    <footer className="social-footer">
+      <div className="social-footer-left">
+        <a href="#">Logo</a>
       </div>
-      <div className="Footer">
-        <div className="Fcard">
-          COPY
-        </div>
-        <div className="Fcard">
-          TERM
-        </div>
+      <div className="social-footer-icons">
+        <ul className="menu-simple">
+          <li>
+            <a
+              href="https://es-la.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillFacebook className="Rrss" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillInstagram className="Rrss" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillLinkedin className="Rrss" />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+              <AiFillTwitterSquare className="Rrss" />
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
-  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
