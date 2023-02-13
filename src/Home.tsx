242144901f8a7099151768f1d4ed1464d0e01e07
Benjaex2 <benjaex2@gmail.com>
2023-02-13 10:17:51 -0300

    fix: Carosul time out

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Carousel from './components/Carousel'
-import Card from "./components/Card"
-import './components/css/index.sass'
+import React from "react";
+import Carousel from "./components/Carousel";
+import Card from "./components/Card";
+import "./components/css/index.sass";
 
 function Home() {
   return (
     <>
-      <Carousel />
+      <Carousel autoPlay={true} showButtons={true} />
       <Card />
     </>
   );
